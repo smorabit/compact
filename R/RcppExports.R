fac129ad2d9f7234ba8314d709825334cf1c5cff
smorabit <smorabit82@gmail.com>
2026-04-13 19:08:43 +0200

    testing cpp function

--- R/RcppExports.R
+++ R/RcppExports.R
@@ -2,6 +2,6 @@
 # Generator token: 10BE3573-1514-4C36-9D1C-5A225CD40393
 
 SparseColDeltaCor <- function(e, d, knn) {
-    .Call('_compact_SparseColDeltaCor', PACKAGE = 'compact', e, d, knn)
+    .Call(`_compact_SparseColDeltaCor`, e, d, knn)
 }
 
